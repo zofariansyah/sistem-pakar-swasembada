09fedf1f85f4995094dff956410e52f2ae9e2e85
Wesclic Developer <wesclicdev@gmail.com>
2023-05-10 11:40:08 +0700

    feat(init): init project and db

---
+++ database/migrations/2023_05_10_043807_tanaman_datas.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tanaman_datas', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_tanaman');
+            $table->string('kelembapan');
+            $table->string('intensitas_penyinaran');
+            $table->string('curah_hujan');
+            $table->string('ph_tanah');
+            $table->string('suhu');
+            $table->string('ketinggian');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tanaman_datas');
+    }
+};
